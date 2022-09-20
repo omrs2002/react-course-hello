ee7c6f746002da19f447f5de4e5a751bb13b1607
Omar Abuhadid <omrs2002@yahoo.com>
2022-09-20 11:16:03 +0300

    Style Employee

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import './Index.css';
 import Employee from './Component/Employee';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
     const showEmployees = true;
     const [role, SetRole] = useState('dev');
     const [employees, setEmployees] = useState([
-        {id:1, name: 'a1', role: 'r1' },
-        {id:2, name: 'a2', role: 'r2' },
-        {id:3, name: 'a3', role: 'r3' },
-        {id:4, name: 'a4', role: 'r4' },
+      {
+        name: 'Caleb',
+        role: 'YouTube Sensation',
+        img: 'https://cdn4.iconfinder.com/data/icons/sketchy-basic-icons/95/smile-128.png',
+    },
+    {
+        name: 'Sal',
+        role: 'Manager',
+        img: 'https://cdn2.iconfinder.com/data/icons/avatars-60/5985/19-Journalist-128.png',
+    },
+    {
+        name: 'John',
+        role: 'Director of Eng.',
+        img: 'https://cdn2.iconfinder.com/data/icons/avatars-60/5985/36-Grandfather-128.png',
+    },
+    {
+        name: 'Melanie',
+        role: 'Software Engineer',
+        img: 'https://cdn4.iconfinder.com/data/icons/sketchy-basic-icons/95/smile-128.png',
+    },
+    {
+        name: 'Corey',
+        role: 'The Devops Guy',
+        img: 'https://cdn2.iconfinder.com/data/icons/avatars-60/5985/19-Journalist-128.png',
+    },
+    {
+        name: 'Jake',
+        role: 'Senior',
+        img: 'https://cdn2.iconfinder.com/data/icons/avatars-60/5985/36-Grandfather-128.png'
+    }
     ]);
     const listItems = employees.map((emp) => {
-        return <Employee key={emp.id} name={emp.name} role={emp.role} />;
+      console.log(emp.id);
+        return <Employee key={uuidv4()} name={emp.name} role={emp.role} img={emp.img} />;
     });
 
+    
     return (
         <div className="App">
             <header className="App-header">
@@ -33,7 +62,7 @@ function App() {
                                 className="text-1xl font-bold bg-gray-300"
                             ></input>
                             <br />
-                            <Employee name="Omar Abuhadid" role="Engineer" />
+                            <Employee name="Omar Abuhadid" role={role} img='https://cdn4.iconfinder.com/data/icons/sketchy-basic-icons/95/smile-128.png' />
                         </>
                     ) : (
                         <p>no Employees!</p>
--- src/Component/Employee.js
+++ src/Component/Employee.js
@@ -1,12 +1,20 @@
 function Employee(props) {
     return(
-        <>
-        <div className="rounded-lg bg-blue-100 m-5">
-        <h3> Eployee Name : {props.name} -  {props.role ? <p>Role is : {props.role}</p> : <p>--no role--</p>} </h3>
-        <br/>
-        <button className="px-4">Update</button>
+        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img alt=""
+            className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+            src={props.img}
+        />
+        <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+                <p className="text-lg text-black font-semibold">{props.name}</p>
+                <p className="text-slate-500 font-medium">{props.role}</p>
+            </div>
+            <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                Update
+            </button>
         </div>
-        </>
+    </div>
     );
 }
 export default Employee;

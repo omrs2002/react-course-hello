a1a7dca2546de506a32e94517b22ee5e8cc9e24b
Omar Abuhadid <omrs2002@yahoo.com>
2022-10-10 13:02:29 +0300

    Add Delete

--- src/Pages/Customer.js
+++ src/Pages/Customer.js
@@ -1,16 +1,55 @@
 import { useParams, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-//import NotFound from '../components/NotFound';
 import { baseUrl } from '../shared';
-
+import Button from 'react-bootstrap/Button';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 export default function Customer() {
     const { id } = useParams();
-    //const navigate = useNavigate(); //not used
+    const navigate = useNavigate();
     const [customer, setCustomer] = useState();
     const [notFound, setNotFound] = useState();
+    function callDelete() {
+        const url = baseUrl + 'customers/' + id;
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong');
+                }
+                navigate('/customers');
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
+    function handleDelete() {
+        confirmAlert({
+            title: 'Confirm to submit',
+            message: 'Are you sure to do delete?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => {
+                        callDelete();
+                    },
+                },
+                {
+                    label: 'No',
+                    onClick: () => {},
+                },
+            ],
+        });
+    }
     useEffect(() => {
-        const url =  baseUrl+'Customers/' + id;
+        const url = baseUrl + 'Customers/' + id;
         console.log(url);
         fetch(url)
             .then((response) => {
@@ -30,11 +69,18 @@ export default function Customer() {
             {notFound ? <p>The customer with id {id} was not found</p> : null}
 
             {customer ? (
-                <div>
-                    <p>{customer.id}</p>
-                    <p>{customer.name}</p>
-                    <p>{customer.industry}</p>
-                </div>
+                <>
+                    <div>
+                        <p>{customer.id}</p>
+                        <p>{customer.name}</p>
+                        <p>{customer.industry}</p>
+                    </div>
+                    <Button variant="danger" onClick={handleDelete}>
+                        Delete
+                    </Button>
+                    <br/>
+                    <hr/>
+                </>
             ) : null}
             <Link to="/customers">Go back</Link>
         </>
